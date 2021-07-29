0ea0ec6c5dff6f2f205e07d86af4d3ed3a848efb
Aukerman <aukermaa@pllimsksparky3.mskcc.org>
2021-07-29 14:53:07 -0400

    Fully removed spark

--- luna_pathology/point_annotation/proxy_table/generate.py
+++ luna_pathology/point_annotation/proxy_table/generate.py
@@ -1,25 +1,27 @@
+import os, json, shutil, click
 
-import os, json
-import shutil
-
-import click
-from pyspark.sql.functions import lit, udf, explode, array, to_json
-from pyspark.sql.types import ArrayType, StringType, IntegerType, MapType, StructType, StructField
-
-from luna_core.common.CodeTimer import CodeTimer
-from luna_core.common.config import ConfigSet
-from luna_core.common.custom_logger import init_logger
-from luna_core.common.sparksession import SparkConfig
-from luna_core.common.utils import get_absolute_path
-from luna_pathology.common.slideviewer_client import fetch_slide_ids
 import luna_core.common.constants as const
+from luna_core.common.CodeTimer import CodeTimer
+from luna_core.common.config    import ConfigSet
 
-logger = init_logger()
+from luna_pathology.common.slideviewer_client import fetch_slide_ids, download_sv_point_annotation
+
+# Dask imports
+import dask
+import dask.dataframe as dd
+from dask.distributed import Client
+from luna_core.common.dask import prune_empty_delayed
+
+# Logging imports
+import logging
+from luna_core.common.custom_logger import init_logger
+logger = init_logger(level=logging.INFO)
 
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
-
-def download_point_annotation(slideviewer_url, slideviewer_path, project_id, user):
+# Special function for this CLI
+import pandas as pd
+def download_point_annotation(slide_id, slideviewer_path, user, project_id, slideviewer_url):
     """Downloads point-click nuclear annotations using slideviewer API
 
     Args:
@@ -31,16 +33,18 @@ def download_point_annotation(slideviewer_url, slideviewer_path, project_id, use
     Returns:
         json: point-click nuclear annotations
     """
-    from slideviewer_client import download_sv_point_annotation
-
-    print (f" >>>>>>> Processing [{slideviewer_path}] <<<<<<<<")
-
     url = slideviewer_url + "/slides/" + str(user) + "@mskcc.org/projects;" + \
           str(project_id) + ';' + slideviewer_path + "/getSVGLabels/nucleus"
-    print(url)
 
-    return download_sv_point_annotation(url)
+    data = download_sv_point_annotation(url)
 
+    if data == None: return None
+
+    df = pd.DataFrame(data)
+    df["slide_id"] = slide_id
+    df = df.set_index("slide_id")
+
+    return df
 
 @click.command()
 @click.option('-d', '--data_config_file', default=None, type=click.Path(exists=True),
@@ -96,16 +100,26 @@ def cli(data_config_file, app_config_file):
         logger.info('app config file: ' + app_config_file)
 
         # load configs
-        cfg = ConfigSet(name=const.DATA_CFG, config_file=data_config_file)
-        cfg = ConfigSet(name=const.APP_CFG,  config_file=app_config_file)
+        cfg = ConfigSet(name="DATA_CFG", config_file=data_config_file)
+        cfg = ConfigSet(name="APP_CFG",  config_file=app_config_file)
 
         # copy app and data configuration to destination config dir
         config_location = const.CONFIG_LOCATION(cfg)
         os.makedirs(config_location, exist_ok=True)
 
-        shutil.copy(app_config_file, os.path.join(config_location, "app_config.yaml"))
-        shutil.copy(data_config_file, os.path.join(config_location, "data_config.yaml"))
-        logger.info("config files copied to %s", config_location)
+        if app_config_file==os.path.join(config_location, "app_config.yaml"):
+            logger.info ("Using cached app_config, skipping copy (possible user permission issue)!")
+        else: 
+            shutil.copy(app_config_file, os.path.join(config_location, "app_config.yaml"))
+            logger.info(f"{app_config_file} file copied to {config_location}")
+
+
+        if data_config_file==os.path.join(config_location, "data_config.yaml"):
+            logger.info ("Using cached data_config, skipping copy (possible user permission issue)!")
+        else: 
+            shutil.copy(data_config_file, os.path.join(config_location, "data_config.yaml"))
+            logger.info(f"{data_config_file} file copied to {config_location}")
+
 
         create_proxy_table()
 
@@ -121,54 +135,49 @@ def create_proxy_table():
 
     cfg = ConfigSet()
 
-    spark = SparkConfig().spark_session(config_name=const.APP_CFG, app_name="luna_pathology.point_annotation.proxy_table.generate")
+    client = Client(n_workers=4, threads_per_worker=4)
+    client.run(init_logger)
+    logger.info (f"Initialized dask client: {client}")
 
     # load paths from configs
-    point_table_path = const.TABLE_LOCATION(cfg)
+    POINT_TABLE_PATH = const.TABLE_LOCATION(cfg)
 
-    PROJECT_ID = cfg.get_value(path=const.DATA_CFG+'::PROJECT_ID')
-    SLIDEVIEWER_URL = cfg.get_value(path=const.DATA_CFG+'::SLIDEVIEWER_URL')
+    PROJECT_ID      = cfg.get_value(path='DATA_CFG::PROJECT_ID')
+    SLIDEVIEWER_URL = cfg.get_value(path='DATA_CFG::SLIDEVIEWER_URL')
 
     # Get slide list to use
     # Download CSV file in the project configs dir
-    slides = fetch_slide_ids(SLIDEVIEWER_URL, PROJECT_ID, const.CONFIG_LOCATION(cfg),
-                             cfg.get_value(path=const.DATA_CFG+'::SLIDEVIEWER_CSV_FILE'))
-    logger.info(slides)
+    df_slides = fetch_slide_ids(
+        SLIDEVIEWER_URL, 
+        PROJECT_ID, const.CONFIG_LOCATION(cfg),
+        cfg.get_value(path='DATA_CFG::SLIDEVIEWER_CSV_FILE'), 
+        as_df=True)
 
-    schema = StructType([StructField("slideviewer_path", StringType()),
-                         StructField("slide_id", StringType()),
-                         StructField("sv_project_id", IntegerType())
-                         ])
-    df = spark.createDataFrame(slides, schema)
-    # populate columns
-    df = df.withColumn("users", array([lit(user) for user in cfg.get_value(const.DATA_CFG+'::USERS')]))
-    df = df.select("slideviewer_path", "slide_id", "sv_project_id", explode("users").alias("user"))
+    l_users  = cfg.get_value('DATA_CFG::USERS')
 
-    # download slide point annotation jsons
-    # example point json:
-    # [{"project_id":"8","image_id":"123.svs","label_type":"nucleus","x":"1440","y":"747","class":"0","classname":"Tissue 1"},{"project_id":"8","image_id":"123.svs","label_type":"nucleus","x":"1424","y":"774","class":"3","classname":"Tissue 4"}]
-    point_json_struct = ArrayType(
-        MapType(StringType(), StringType())
-    )
-    spark.sparkContext.addPyFile(get_absolute_path(__file__, "../../common/slideviewer_client.py"))
-    download_point_annotation_udf = udf(download_point_annotation,  point_json_struct)
+    logger.info("Got slide listing!!")
+    logger.info(df_slides)
+    logger.info(l_users)
 
-    df = df.withColumn("sv_json",
-                       download_point_annotation_udf(lit(SLIDEVIEWER_URL), "slideviewer_path", "sv_project_id", "user"))\
-        .cache()
-    # drop empty jsons that may have been created
-    df = df.dropna(subset=["sv_json"])
+    # Prepare
+    d_download_point_annotation = dask.delayed(download_point_annotation)
 
-    # populate "date_added", "date_updated","latest", "sv_json_record_uuid"
-    spark.sparkContext.addPyFile(get_absolute_path(__file__, "../../common/EnsureByteContext.py"))
-    spark.sparkContext.addPyFile(get_absolute_path(__file__, "../../common/utils.py"))
-    from utils import generate_uuid_dict
-    sv_json_record_uuid_udf = udf(generate_uuid_dict, StringType())
+    outputs = []
+    logger.info(f"Submitting {len(df_slides) * len(l_users)} tasks!!")
+    for _, row in df_slides.iterrows():
+        for user in l_users:
+            outputs.append( d_download_point_annotation(row.slide_id, row.slideviewer_path, user, PROJECT_ID, SLIDEVIEWER_URL) )
+    
+    # We have to prune our tasks for only filled dataframes
+    ddf = dd.from_delayed(prune_empty_delayed(outputs))
+    print (ddf.head())
 
-    df = df.withColumn("sv_json_record_uuid", sv_json_record_uuid_udf(to_json("sv_json"), array(lit("SVPTJSON"))))
+    POINT_TABLE_PATH = POINT_TABLE_PATH + "_TESTING_DEV/" # TO REMOVE
 
-    df.show(10, False)
-    df.write.format("parquet").mode("overwrite").save(point_table_path)
+    logger.info(f"Saving table -> {POINT_TABLE_PATH}")
+
+    # Now we can use dasks's paralellism in the backend to write our tables!
+    ddf.to_parquet(POINT_TABLE_PATH)
 
 
 if __name__ == "__main__":
