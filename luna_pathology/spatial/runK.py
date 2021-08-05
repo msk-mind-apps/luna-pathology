a8166b3bbef8c6c10d9103c7b1286d40f53e45db
Shabet <shabetm@pllimsksparky3.mskcc.org>
2021-08-05 16:05:44 -0400

    Update RunK

--- luna_pathology/spatial/runK.py
+++ luna_pathology/spatial/runK.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import scipy.stats
 import numpy as np
+from copy import deepcopy
 # stats = __import__("luna-pathology.luna_pathology.spatial.stats", fromlist=[None])
 from stats import *
 
@@ -17,20 +18,21 @@ def runKfunction(detection_path,phenotype_path,method_data):
 
 	print(f"Number of datapoints: {len(dfiles)}")
 
-	# Create the data structure to store K function results
+	# Load the metadata and set up the data structure
 	data = {}
-	for i in range(len(dfiles)):
-		patient = dfiles[i].split("_")[0]
+	metadata = {}
+	fovmeta = pd.read_csv("fov_metadata.tsv", sep='\t')
+	patient_id = fovmeta["patient_id"]
+	tumor_site_short = fovmeta["tumor_site_short"]
+	isabl_id = fovmeta["isabl_id"]
+	for patient in patient_id:
 		data[patient] = {}
-	for i in range(len(dfiles)):
-		labels = dfiles[i].split("_")
-		patient = labels[0]
-		region = labels[1]
-		data[patient][region] = []
+	for i in range(len(patient_id)):
+		data[patient_id[i]][tumor_site_short[i]] = []
+		metadata[isabl_id[i]] = {"patient":patient_id[i],"site":tumor_site_short[i]}
+	data = {"count":deepcopy(data),"intensity":deepcopy(data),"distance":deepcopy(data)}
 
-	print(f"Number of patients: {len(data)}")
-
-	# Loop through each file and fill out the data structure
+	# Loop through each file and compute the K function
 	print("Computing the K function...")
 	for i in range(len(dfiles)):
 		print(i)
@@ -49,6 +51,9 @@ def runKfunction(detection_path,phenotype_path,method_data):
 		val1 = method_data["phenotype1"]["value"]
 		col2 = method_data["phenotype2"]["name"]
 		val2 = method_data["phenotype2"]["value"]
+		R = method_data["R"]
+		count = method_data["count"]
+		distance = method_data["distance"]
 
 		# Create the data arrays
 		pheno1 = df[df[col1] == val1]
@@ -59,79 +64,52 @@ def runKfunction(detection_path,phenotype_path,method_data):
 		assert (p1XY.size != 0), f"List of phenotype 1 cells ({val1}) is empty"
 		assert (p2XY.size != 0), f"List of phenotype 2 cells ({val2}) is empty"
 
-		# Identify the datapoint
-		labels = dfiles[i].split("_")
-		patient = labels[0]
-		region = labels[1]
-		fov = labels[4]
+		patient_metadata = metadata[df["Image"][0]]
 
 		# Compute the K function
-		k = Kfunction(p1XY,p2XY,60,ls=True,intensity=I)
+		K = Kfunction(p1XY,p2XY,R,ls=True,count=count,intensity=I,distance=distance)
+		
+		# Unpack and fill out the aggregation data structure
+		keys = K.keys()
+		for key in keys:
+			data[key][patient_metadata["patient"]][patient_metadata["site"]].extend(K[key])
 
-		assert(0==5)
-
-		d = {
-			"Moment":np.array([np.mean(k),np.var(k),scipy.stats.skew(k),scipy.stats.kurtosis(k)]),
-			"N":len(p1XY)
-		}
-		data[patient][region].append(d)
-
-	# Perform the aggregation across replicates in a region weighting by |p1XY|
+	# Perform the aggregation across replicates in a region
 	print("Performing aggregation")
-	for i in data:
-		for j in data[i]:
-			weight = 0
-			moments = np.array([0.0,0.0,0.0,0.0])
-			for k in data[i][j]:
-				weight += k["N"]
-				moments += k["Moment"] * k["N"]
-			moments /= weight
-			data[i][j] = moments
+	for key in keys:
+		for patient in data[key]:
+			for region in data[key][patient]:
+				arr = data[key][patient][region]
+				moments = np.array([np.mean(arr),np.var(arr),scipy.stats.skew(arr),scipy.stats.kurtosis(arr)])
+				data[key][patient][region] = moments
 
-	for i in data:
-		print(i)
-		for j in data[i]:
-			print(f"  {j}: {data[i][j]}")
+	# Save the output
+	title = f"p1={val1},p2={val2},I={method_data['intensity']},R={R}.npy"
+	np.save(title,  data) 
 
 
+if __name__ == "__main__":
+	dr = "/gpfs/mskmind_ess/vazquezi/data/transfers/spectrum/results/mpif/v11"
+	d_path = dr + "/qupath/outputs/format_detection/fov"
+	p_path = dr + "/cell-type/outputs/apply_thresholds/fov"
+	p1 = {
+		"name" : "cell_type",
+		'value' : 'panCK+'
+	}
+	p2 = {
+		"name" : "CD68_state",
+		'value' : 'CD68+'
+	}
+	method_data = {
+		"phenotype1" : p1,
+		"phenotype2" : p2,
+		"R" : 60,
+		"count" : True,
+		"intensity" : 'Cell: PDL1 mean',
+		"distance" : False
+	}
 
-
-
-
-
-
-
-
-
-
-
-dr = "/gpfs/mskmind_ess/vazquezi/data/transfers/spectrum/results/mpif/v11"
-d_path = dr + "/qupath/outputs/format_detection/fov"
-p_path = dr + "/cell-type/outputs/apply_thresholds/fov"
-p1 = {
-	"name" : "cell_type",
-	'value' : 'panCK+'
-}
-p2 = {
-	"name" : "CD68_state",
-	'value' : 'CD68+'
-}
-method_data = {
-	"phenotype1" : p1,
-	"phenotype2" : p2,
-	"intensity" : 'Cell: PDL1 mean'
-
-}
-runKfunction(d_path, p_path, method_data)
-
-
-
-
-
-
-
-
-
+	runKfunction(d_path, p_path, method_data)
 
 
 
