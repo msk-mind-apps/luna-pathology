6d381772c8a1ccd0bebf835aaad58bfd48d2e5d4
Shabet <shabetm@pllimsksparky3.mskcc.org>
2021-08-09 16:59:07 -0400

    Improve runK, preliminary analysis

--- luna_pathology/spatial/analysis.py
+++ luna_pathology/spatial/analysis.py
@@ -1,17 +1,93 @@
 import numpy as np
+import pandas as pd
+import scipy.stats
+
+fovmeta = pd.read_csv("fov_metadata.tsv", sep='\t')
+samplemeta = pd.read_csv("sample_metadata.tsv", sep='\t')
+samplemetadata = dict(zip(samplemeta["pici_id"], samplemeta.to_dict('records')))
 
 val1 = "panCK+"
-val2 = "CD68+"
+val2 = "CD8+"
 I = "Cell: PDL1 mean"
+filename = f"p1={val1},p2={val2},I={I}.tsv"
 R = "60"
+moments = ["mean","variance","skew","kurtosis"]
 
-title = f"p1={val1},p2={val2},I={I},R={R}.npy"
-data = np.load(title, allow_pickle=True).item()
+data = pd.read_csv(filename, sep='\t')
+data = data[data.radius == 60]
+countdata = data[data.function == "count"]
+intensitydata = data[data.function == "intensity"]
 
-for key in data:
-	print(key)
-	for patient in data[key]:
-		print(f"  {patient}")
-		for site in data[key][patient]:
-			print(f"    {site}: {data[key][patient][site]}")
+# 1. The effect of site specificity on cell-cell interactions
+# This you can study by joining the tumor_supersite column in
+# sample_metadata, and aggregating K-statistics across FOVs based
+# on that variable
+
+
+sitelist = ["Adnexa", "Omentum", "Upper Quadrant", "Peritoneum", "Bowel", "Other"]
+sites = [samplemetadata[i]["tumor_supersite"] for i in countdata.pici_id]
+
+arr = intensitydata
+
+for i in range(5):
+	for j in range(5-i):
+		s1 = sitelist[i]
+		s2 = sitelist[i+j+1]
+
+		type1 = arr[[s == s1 for s in sites]]
+		type2 = arr[[s == s2 for s in sites]]
+
+		for k in range(4):
+			a = type1[moments[k]]
+			b = type2[moments[k]]
+			p = scipy.stats.ttest_ind(a,b).pvalue
+			print(f"{p}: {s1} {s2} {moments[k]}")
+
+
+# 2. The effect of DNA repair deficiency in tumour cells of a
+# subset of these patients. These are best identified either
+# based on BRCA1/2 mutations (e.g. from IMPACT) or using
+# whole-genome sequencing to infer signatures of DNA repair
+# deficiency. You should be able to join the table in this git
+# repo by patient_id, and then use the consensus_signature and
+# consensus_signature_short columns to try and look for
+# associations based on the K-statistic
+# (https://github.com/shahcompbio/spectrum-tme/blob/master/tables/mutational_signatures.xlsx).
+
+
+
+
+
+
+
+# Metastatic vs Primary
+
+arr = intensitydata
+m = [samplemetadata[i]["tumor_type"]=="Metastasis" for i in countdata.pici_id]
+p = [samplemetadata[i]["tumor_type"]=="Primary" for i in countdata.pici_id]
+
+m = arr[m]
+p = arr[p]
+
+for k in range(4):
+	a = m[moments[k]]
+	b = p[moments[k]]
+	pval = scipy.stats.ttest_ind(a,b).pvalue
+	print(f"{pval}: {moments[k]}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- luna_pathology/spatial/runK.py
+++ luna_pathology/spatial/runK.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import scipy.stats
 import numpy as np
-from copy import deepcopy
+import csv
 # stats = __import__("luna-pathology.luna_pathology.spatial.stats", fromlist=[None])
 from stats import *
 
@@ -18,24 +18,38 @@ def runKfunction(detection_path,phenotype_path,method_data):
 
 	print(f"Number of datapoints: {len(dfiles)}")
 
-	# Load the metadata and set up the data structure
-	data = {}
-	metadata = {}
+	# Load the metadata files
 	fovmeta = pd.read_csv("fov_metadata.tsv", sep='\t')
-	patient_id = fovmeta["patient_id"]
-	tumor_site_short = fovmeta["tumor_site_short"]
-	isabl_id = fovmeta["isabl_id"]
-	for patient in patient_id:
-		data[patient] = {}
-	for i in range(len(patient_id)):
-		data[patient_id[i]][tumor_site_short[i]] = []
-		metadata[isabl_id[i]] = {"patient":patient_id[i],"site":tumor_site_short[i]}
-	data = {"count":deepcopy(data),"intensity":deepcopy(data),"distance":deepcopy(data)}
+	samplemeta = pd.read_csv("sample_metadata.tsv", sep='\t')
+	
+	# Turn the metadata files into dictionaries so we can look up information later
+	fovmetadata = dict(zip(fovmeta["isabl_id"],fovmeta.to_dict('records')))
+	samplemetadata = dict(zip(samplemeta["pici_id"], samplemeta.to_dict('records')))
+
+	# Unpack the method_data dictionary
+	col1 = method_data["phenotype1"]["name"]
+	val1 = method_data["phenotype1"]["value"]
+	col2 = method_data["phenotype2"]["name"]
+	val2 = method_data["phenotype2"]["value"]
+	R = method_data["R"]
+	count = method_data["count"]
+	distance = method_data["distance"]
+
+	# Check if the computation has been run with this R before
+	filename = f"p1={val1},p2={val2},I={method_data['intensity']}.tsv"
+	if os.path.exists(filename):
+		file_df = pd.read_csv(filename, sep='\t')
+		Rs = set(file_df["radius"])
+		if R in Rs:
+			msg = f"This computation has already been run for R = {R}"
+			assert 0, msg
 
 	# Loop through each file and compute the K function
 	print("Computing the K function...")
+	data = {"count":{},"intensity":{},"distance":{}}
 	for i in range(len(dfiles)):
 		print(i)
+
 		dpath = detection_path + "/" + dfiles[i] + "/object_detection_results.tsv"
 		ppath = phenotype_path + "/" + pfiles[i] + "/phenotypes.tsv"
 
@@ -46,14 +60,16 @@ def runKfunction(detection_path,phenotype_path,method_data):
 		p = p.drop(columns=["Image", "Name", "Class", "Parent", "ROI"])
 		df = pd.merge(df, p, on='cell_id',  how='left')
 
-		# Unpack the method_data dictionary
-		col1 = method_data["phenotype1"]["name"]
-		val1 = method_data["phenotype1"]["value"]
-		col2 = method_data["phenotype2"]["name"]
-		val2 = method_data["phenotype2"]["value"]
-		R = method_data["R"]
-		count = method_data["count"]
-		distance = method_data["distance"]
+		# Look up the metadata
+		isabl_id = df["Image"][0]
+		fovmeta = fovmetadata[isabl_id]
+		pici_id = fovmeta["pici_id"]
+		samplemeta = samplemetadata[pici_id]
+
+		# Filter for qc_status == 'Pass' in sample_metadata and
+		# fov_status == 'Pass' in fov_metadata
+		if (fovmeta["fov_status"] != "Pass") or (samplemeta["qc_status"] != "Pass"):
+			continue
 
 		# Create the data arrays
 		pheno1 = df[df[col1] == val1]
@@ -61,33 +77,46 @@ def runKfunction(detection_path,phenotype_path,method_data):
 		p1XY = np.array(pheno1[["Centroid X µm","Centroid Y µm"]])
 		p2XY = np.array(pheno2[["Centroid X µm","Centroid Y µm"]])
 		I = np.array(pheno2[method_data["intensity"]])
-		assert (p1XY.size != 0), f"List of phenotype 1 cells ({val1}) is empty"
-		assert (p2XY.size != 0), f"List of phenotype 2 cells ({val2}) is empty"
-
-		patient_metadata = metadata[df["Image"][0]]
+		if p1XY.size == 0:
+			print(f"WARNING: List of phenotype 1 cells ({val1}) is empty for {isabl_id}")
+		if p2XY.size == 0:
+			print(f"WARNING: List of phenotype 2 cells ({val2}) is empty for {isabl_id}")
 
 		# Compute the K function
 		K = Kfunction(p1XY,p2XY,R,ls=True,count=count,intensity=I,distance=distance)
-		
-		# Unpack and fill out the aggregation data structure
-		keys = K.keys()
-		for key in keys:
-			data[key][patient_metadata["patient"]][patient_metadata["site"]].extend(K[key])
 
-	# Perform the aggregation across replicates in a region
-	print("Performing aggregation")
-	for key in keys:
-		for patient in data[key]:
-			for region in data[key][patient]:
-				arr = data[key][patient][region]
-				moments = np.array([np.mean(arr),np.var(arr),scipy.stats.skew(arr),scipy.stats.kurtosis(arr)])
-				data[key][patient][region] = moments
+		# Unpack and fill out the aggregation data structure
+		for key in K:
+			if pici_id in data[key]:
+				np.append(data[key][pici_id],K[key])
+			else:
+				data[key][pici_id] = K[key]
 
 	# Save the output
-	title = f"p1={val1},p2={val2},I={method_data['intensity']},R={R}.npy"
-	np.save(title,  data) 
+	dataframe = []
+	for key in data:
+		for pici_id in data[key]:
+			arr = data[key][pici_id]
+			dataframe.append({
+				"pici_id":pici_id,
+				"radius":R,
+				"function":key,
+				"mean":np.mean(arr),
+				"variance":np.var(arr),
+				"skew":scipy.stats.skew(arr),
+				"kurtosis":scipy.stats.kurtosis(arr)
+			})
+
+	# Store the moments in a new/existing .tsv
+	output_file = open(filename, 'a+')
+	dict_writer = csv.DictWriter(output_file, dataframe[0].keys(), delimiter='\t')
+	if os.stat(filename).st_size == 0:
+		dict_writer.writeheader()	
+	dict_writer.writerows(dataframe)
+	output_file.close()
 
 
+# Driver function to produce the .tsv output
 if __name__ == "__main__":
 	dr = "/gpfs/mskmind_ess/vazquezi/data/transfers/spectrum/results/mpif/v11"
 	d_path = dr + "/qupath/outputs/format_detection/fov"
@@ -97,8 +126,8 @@ if __name__ == "__main__":
 		'value' : 'panCK+'
 	}
 	p2 = {
-		"name" : "CD68_state",
-		'value' : 'CD68+'
+		"name" : "cell_type",
+		'value' : 'CD8+'
 	}
 	method_data = {
 		"phenotype1" : p1,
